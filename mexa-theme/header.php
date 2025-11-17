e3b5bd6af864b043c4b22f4f1b31f4657f0a3ecb
dimich² <164957381+samboryk@users.noreply.github.com>
2025-11-17 12:39:15 +0200

    Fix navigation links

--- mexa-theme/header.php
+++ mexa-theme/header.php
@@ -4,6 +4,37 @@
  *
  * @package MEXA_Infrastructure
  */
+
+if ( ! function_exists( 'mexa_get_template_page_link' ) ) {
+    /**
+     * Resolve the permalink for the first page that uses the provided template.
+     *
+     * This allows the navigation to stay in sync regardless of the actual page
+     * slugs that the site owner selects inside the WordPress admin.
+     *
+     * @param string $template Template filename, e.g. 'link.php'.
+     * @param string $fallback Relative fallback path if no page uses the template.
+     *
+     * @return string Permalink for the matching page or a fallback URL.
+     */
+    function mexa_get_template_page_link( $template, $fallback = '/' ) {
+        $pages = get_pages(
+            [
+                'meta_key'   => '_wp_page_template',
+                'meta_value' => $template,
+                'number'     => 1,
+                'post_type'  => 'page',
+                'post_status'=> 'publish',
+            ]
+        );
+
+        if ( ! empty( $pages ) && isset( $pages[0]->ID ) ) {
+            return get_permalink( $pages[0]->ID );
+        }
+
+        return home_url( $fallback );
+    }
+}
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -32,9 +63,9 @@
             <nav id="header" class="fixed">
                 <ul>
                     <li style="margin-right: 50px;margin-left: 30px;"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Головна сторінка</a></li>
-                    <li style="margin-right: 50px;"><a href="<?php echo esc_url( home_url( '/link/' ) ); ?>">Інфраструктура</a></li>
-                    <li style="margin-right: 50px;"><a href="<?php echo esc_url( home_url( '/link2/' ) ); ?>">Токен</a></li>
-                    <li style="margin-right: 50px;"><a href="<?php echo esc_url( home_url( '/link3/' ) ); ?>">Навігатор по сайту</a></li>
+                    <li style="margin-right: 50px;"><a href="<?php echo esc_url( mexa_get_template_page_link( 'link.php' ) ); ?>">Інфраструктура</a></li>
+                    <li style="margin-right: 50px;"><a href="<?php echo esc_url( mexa_get_template_page_link( 'link2.php' ) ); ?>">Токен</a></li>
+                    <li style="margin-right: 50px;"><a href="<?php echo esc_url( mexa_get_template_page_link( 'link3.php' ) ); ?>">Навігатор по сайту</a></li>
                 </ul>
             </nav>
 
